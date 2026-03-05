f83250e7422a59b5d12407963f9af880afc9d256
aman-d006 <akd006@gmail.com>
2026-03-05 23:05:31 +0530

    Fix CORS

--- backend/main.py
+++ backend/main.py
@@ -52,6 +52,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))

46837837aa1608512f1adbf629fc278e39869f54
aman-d006 <akd006@gmail.com>
2026-03-05 23:19:54 +0530

    Temporarily allow all origins for CORS testing

--- backend/main.py
+++ backend/main.py
@@ -48,7 +48,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS_LIST,
+    # allow_origins=ALLOWED_ORIGINS_LIST,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

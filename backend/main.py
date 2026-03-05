869ab250318076bdca6b25b547b0b5cc796824fc
aman-d006 <akd006@gmail.com>
2026-03-05 23:30:13 +0530

    Remove all origins for CORS testing

--- backend/main.py
+++ backend/main.py
@@ -49,7 +49,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=ALLOWED_ORIGINS_LIST,
-    allow_origins=["*"],
+    allow_origins=["https://sagago.vercel.app", "http://localhost:5173", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
